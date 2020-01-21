fdafdb09319aad149d487890ddcc0c1f6ef7f445
小天 <cloudin@foxmail.com>
2020-01-21 22:46:11 +0800

    修复与 mybatis-spring-boot-starter 的兼容问题

--- yugong-auth-spring-boot-starter/src/main/java/org/yugong/auth/starter/YgAuthJdbcProviderAutoConfiguration.java
+++ yugong-auth-spring-boot-starter/src/main/java/org/yugong/auth/starter/YgAuthJdbcProviderAutoConfiguration.java
@@ -1,40 +1,40 @@
 package org.yugong.auth.starter;
 
-import org.apache.ibatis.mapping.Environment;
 import org.apache.ibatis.session.Configuration;
 import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
-import org.apache.ibatis.transaction.TransactionFactory;
-import org.apache.ibatis.transaction.jdbc.JdbcTransactionFactory;
+import org.mybatis.spring.boot.autoconfigure.ConfigurationCustomizer;
+import org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.DependsOn;
 import org.yugong.auth.client.YgAuthService;
 import org.yugong.auth.client.YgAuthServiceJdbcImpl;
 import org.yugong.auth.client.mapper.YgAuthMapper;
 
-import javax.sql.DataSource;
-
 /**
+ * 使用基于 jdbc 的权限系统支持。依赖 mybatis-spring-boot-starter
+ *
  * @author 小天
  * @date 2020/1/5 1:21
  */
 @ConditionalOnProperty(value = "yg.auth.provider-jdbc-enable", havingValue = "true")
+@ConditionalOnClass({MybatisAutoConfiguration.class})
+@AutoConfigureAfter({MybatisAutoConfiguration.class})
 public class YgAuthJdbcProviderAutoConfiguration {
+
+    @Bean
+    public ConfigurationCustomizer mybatisConfigurationCustomizer() {
+        return new ConfigurationCustomizer() {
+            @Override
+            public void customize(Configuration configuration) {
+                configuration.addMapper(YgAuthMapper.class);
+            }
+        };
+    }
 
     @Bean
     public YgAuthService ygAuthServiceJdbcImpl(SqlSessionFactory sqlSessionFactory) {
         return new YgAuthServiceJdbcImpl(sqlSessionFactory);
-    }
-
-    @ConditionalOnProperty(value = "yg.auth.provider-jdbc-mybatis-embed-enable", havingValue = "true")
-    @Bean("ygAuthSqlSessionFactory")
-    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) {
-        TransactionFactory transactionFactory = new JdbcTransactionFactory();
-        Environment environment = new Environment("ygAuthDefaultEnv", transactionFactory, dataSource);
-        Configuration configuration = new Configuration(environment);
-        configuration.addMapper(YgAuthMapper.class);
-        return new SqlSessionFactoryBuilder().build(configuration);
     }
 }
